fd32214b71052c03ffe37f4665cc5f4a01bc339e
ynuosoft <3556612756@qq.com>
2018-07-04 19:30:42 +0800

    redis图片

--- src/main/java/com/ynuosoft/redisinfo/monitor/Application.java
+++ src/main/java/com/ynuosoft/redisinfo/monitor/Application.java
@@ -1,5 +1,6 @@
 package com.ynuosoft.redisinfo.monitor;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
